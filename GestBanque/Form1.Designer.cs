8d8f5148e50cd9263a04a3759e23517588f264dc
zaknow <zakaria_kasmi@hotmail.fr>
2017-05-25 13:19:35 +0100

    GestBanqueV0.8

--- GestBanque/Form1.Designer.cs
+++ GestBanque/Form1.Designer.cs
@@ -43,7 +43,7 @@
             this.clientToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(589, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(671, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -72,7 +72,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(589, 367);
+            this.ClientSize = new System.Drawing.Size(671, 367);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Menu";
